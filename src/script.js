402b41cf81df2e83b2e38943e94891a1009c2918
Aaron Enyetu <aaronenyetu@gmail.com>
2023-05-01 14:54:25 -0700

    Added depthColor, surfaceColor, color off set and color multiplier

--- src/script.js
+++ src/script.js
@@ -12,6 +12,7 @@ console.log(waterFragmentShader)
  */
 // Debug
 const gui = new dat.GUI({ width: 340 })
+const debugObject = {}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -25,6 +26,10 @@ const scene = new THREE.Scene()
 // Geometry
 const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
 
+//Color
+debugObject.depthColor = '#186691'
+debugObject.surfaceColor = '#9bd8ff'
+
 // Material
 const waterMaterial = new THREE.ShaderMaterial({
     vertexShader: waterVertexShader,
@@ -34,7 +39,11 @@ const waterMaterial = new THREE.ShaderMaterial({
         uTime: { value: 0 },
         uBigWavesElevation: { value: 0.2 },
         uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
-        uBigWavesSpeed: { value: 0.75 }
+        uBigWavesSpeed: { value: 0.75 },
+        uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
+        uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
+        uColorOffSet: { value: 0.08 },
+        uColorMultiplier: { value: 5 }
     }
 })
 
@@ -42,6 +51,10 @@ gui.add(waterMaterial.uniforms.uBigWavesElevation, 'value').min(0).max(1).step(0
 gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x').min(0).max(10).step(0.001).name('uBigWavesFrequencyX')
 gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y').min(0).max(10).step(0.001).name('uBigWavesFrequencyY')
 gui.add(waterMaterial.uniforms.uBigWavesSpeed, 'value').min(0).max(4).step(0.001).name('uBigWavesSpeed')
+gui.addColor(debugObject, 'depthColor').onChange(() => { waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor) })
+gui.addColor(debugObject, 'surfaceColor').onChange(() => { waterMaterial.uniforms.uSurfaceColor.value.set(debugObject.depthColor) })
+gui.add(waterMaterial.uniforms.uColorOffSet, 'value').min(0).max(1).step(0.001).name('uColorOffSet')
+gui.add(waterMaterial.uniforms.uColorMultiplier, 'value').min(0).max(1).step(0.001).name('uColorMultiplier')
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
